3b5e4b2d4a0ef52cea484257b2ad1113526ff7a5
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2020-07-08 19:02:28 -0400

    sprint is ready for review

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ console.log(artists[2]["bio"]);
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, 
 Vincent Van Gogh is currently Vincent Van Dough. 
 Use an array method to fix this issue and console.log() to check your work. */
-
+artists[8].name = "Vincent Van Gogh"
 console.log(artists[8].name);
 
 
@@ -225,10 +225,11 @@ console.log(artists[8].name);
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-// function getArtistByIndex(array, id) {
-//   console.log(`The arstist at the index ${id} is ${array[id].name}`);
-// }
-//  getArtistByIndex(artists[0]);
+function getArtistByIndex(array){
+
+  console.log(`The arstist at the index ${array.id} is ${array.name}`);
+}
+ getArtistByIndex(artists[0]);
 
 //   /**
 
